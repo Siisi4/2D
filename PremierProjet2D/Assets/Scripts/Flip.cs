b77499973ebb14c48f01e16b397f008a7fb3f186
Siisi4 <siisi4officiel@gmail.com>
2020-07-03 13:52:03 +0200

    plateforme et personnage qui tombe

---
+++ PremierProjet2D/Assets/Scripts/Flip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public GameObject drapeau;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 contactPoint = collision.GetContact(0).point;
+        contactPoint.z = -5;
+
+        // on va instancier le drapeau en un point donné.
+        Instantiate(drapeau, contactPoint, Quaternion.identity);
+    }
+
+
+
+}
